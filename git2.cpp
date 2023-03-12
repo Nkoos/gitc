79686e9486ab25698ac38052518d6b1b270fea18
nikolay <dvacher12@mail.ru>
2023-03-12 12:11:21 +0400

    First commit

---
+++ git2.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+    int main()
+{
+    double num1{},num2{};
+    std::cin >> num1;cout << "\n";
+    std::cin >> num2;cout << "\n";
+    double num3{pow(num1, 2) * pow(num2, 2)};
+if(num1>0)
+    {
+    if(num2>0)
+        {
+            std::cout << num3;
+        }
+
+    }    
+return 0;
+}
