58672d7c35327fbb8f78620a786779099e6819c3
Nkoos <dvacher12@mail.ru>
2023-03-16 21:04:15 +0400

    AddSquaresNECHtask

---
+++ taskscpp/С/squaresn.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n{};
+    cout  << "Enter limit number" << endl;
+    cin >> n; cout << endl;
+    for (int i{1}; i<=n; i+=2)
+    {
+        cout << ":" << pow(i,2) << endl;
+    
+    }
+
+
+
+
+
+}
